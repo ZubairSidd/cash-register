4eb5f808a5c14dbbaad7e1a59c8fd6b7d1ba5efc
Zubair Siddique <zubairsidd20@gmail.com>
2021-08-27 15:39:55 +0530

    linked js and css to html

---
+++ app.js
@@ -0,0 +1,13 @@
+const checkButton = document.querySelector("#checkButton");
+let billAmountInput = document.querySelector(".billAmount");
+let cashGivenInput = document.querySelector(".cashGiven");
+
+const note = [2000, 500, 100, 20, 10, 5, 1];
+
+function checkButtonHandler() {
+  alert("hello");
+}
+console.log("hello");
+checkButton.addEventListener("click", () => {
+  alert("hello");
+});
